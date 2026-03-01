009d182afd11ed93d4342157e213b4f1a92b4d00
daniel <danemokhor@gmail.com>
2026-03-02 00:16:50 +0100

    cosnole

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,8 @@ async function bootstrap() {
   app.enableCors();
 
   const port = process.env.PORT || 3000;
+  console.log(`Server is running on port ${port}`);
   await app.listen(port, '0.0.0.0');
+  console.log(`Server is running on ${await app.getUrl()}`);
 }
 bootstrap();
